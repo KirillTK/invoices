eceb2d987dabd9da3fdf16b01eb16ec94ff127d1
Kirill Tkachyov <kirill@auxili.us>
2024-07-13 14:26:24 +0200

    update layout

--- src/widgets/invoice-form/invoice-form.tsx
+++ src/widgets/invoice-form/invoice-form.tsx
@@ -3,7 +3,6 @@
 import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { ClientCombobox } from "~/features/client-combobox";
-import { ComboboxField } from "~/shared/components/controls/combobox-field";
 import { DatePickerField } from "~/shared/components/controls/date-picker-field";
 import { InputField } from "~/shared/components/controls/input-field";
 import { Label } from "~/shared/components/label";
@@ -13,19 +12,21 @@ interface InvoiceForm {
   title: string;
   description: string;
   vatInvoice: boolean;
-  dueDate: Date,
-  realizationDate: Date,
-  dateOfIssue: Date,
+  dueDate: Date;
+  realizationDate: Date;
+  dateOfIssue: Date;
 
   //client info
   clientId: string;
   clientAddress: string;
   clientName: string;
+  clientTaxIndex: string;
 
   // user info
   userId: string;
   userAddress: string;
   userName: string;
+  userTaxIndex: string;
 }
 
 export function InvoiceForm() {
@@ -44,32 +45,31 @@ export function InvoiceForm() {
       className="grid grid-cols-2 gap-4 p-4 shadow-[0_0.5em_1.5em_-0.5em_rgba(0,0,0,0.5)]"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <div className="flex flex-col space-y-8">
-        <div>
-          <Label className="mb-4 text-lg font-medium">From:</Label>
-          <ComboboxField
-            options={[]}
-            field={register("userId")}
-            className="mt-4"
-          />
-        </div>
-        <div>
-          <Label className="mb-4 text-lg font-medium">To:</Label>
-          <ClientCombobox field={register("clientId")} />
-        </div>
-      </div>
+      <div></div>
+
       <div>
-        <Label className="mb-4 block text-lg font-medium">
-          Invoice Details
-        </Label>
-        <div className="grid grid-cols-2 gap-4">
-          <DatePickerField field={register("test")} label="Date of issue" />
-          <DatePickerField
-            field={register("test")}
-            label="Realization date of the order"
-          />
-          <InputField field={register("test2")} label="Invoice number" />
-        </div>
+        <DatePickerField
+          field={register("dateOfIssue")}
+          label="Realization date of the order"
+        />
+        <DatePickerField
+          field={register("realizationDate")}
+          label="Realization date of the order"
+        />
+      </div>
+
+      <div className="grid gap-y-2">
+        <Label className="text-lg font-medium">From:</Label>
+        <InputField field={register("userName")} />
+        <InputField field={register("userTaxIndex")} label="NIP/VAT ID:" />
+        <InputField field={register("userAddress")} label="Address:" />
+      </div>
+
+      <div className="grid gap-y-2">
+        <Label className="text-lg font-medium">To:</Label>
+        <ClientCombobox field={register("clientId")} />
+        <InputField field={register("clientTaxIndex")} label="NIP/VAT ID:" />
+        <InputField field={register("clientAddress")} label="Address:" />
       </div>
     </form>
   );
