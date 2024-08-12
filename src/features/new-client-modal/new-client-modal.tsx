ce3ff99257f115fc0e7a9e2266e88edf9cdea414
Kirill Tkachyov <kirill@auxili.us>
2024-08-12 21:17:21 +0200

    create user

--- src/features/new-client-modal/new-client-modal.tsx
+++ src/features/new-client-modal/new-client-modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useUser } from "@clerk/clerk-react";
 import { useSWRConfig } from "swr";
 import type { z } from "zod";
 import type { ClientModel } from "~/entities/client/client.model";
@@ -18,7 +19,9 @@ import { Label } from "~/shared/components/label";
 import { getFormErrorArray, isHttpValidationError } from "~/shared/utils/http";
 import { clientSchema } from "~/shared/schemas/client.schema";
 
-type NewClientForm = z.infer<typeof clientSchema>;
+const newClientModalSchema = clientSchema.omit({ userId: true });
+
+type NewClientForm = z.infer<typeof newClientModalSchema>;
 
 const defaultValues: NewClientForm = {
   name: "",
@@ -27,11 +30,14 @@ const defaultValues: NewClientForm = {
   city: "",
   address: "",
   zip: "",
-  userId: "",
 };
 
 export function NewClientModal() {
   const title = "Create new client";
+
+  const { user } = useUser();
+
+  console.log(user, "!!!!");
 
   const {
     handleSubmit,
@@ -39,20 +45,28 @@ export function NewClientModal() {
     getValues,
     formState: { errors },
     setError,
-  } = useForm<z.infer<typeof clientSchema>>({
+  } = useForm<z.infer<typeof newClientModalSchema>>({
     defaultValues,
-    resolver: zodResolver(clientSchema),
+    resolver: zodResolver(newClientModalSchema),
   });
-
-  console.log(errors, "ERRORS");
 
   const { mutate } = useSWRConfig();
 
   const saveClient = async () => {
+    if (!user) {
+      console.error("No user session");
+      return;
+    }
+
+    const formValues = {
+      ...getValues(),
+      userId: user.id,
+    };
+
     try {
       const response = await fetch("/api/client", {
         method: "POST",
-        body: JSON.stringify(getValues()),
+        body: JSON.stringify(formValues),
       });
 
       if (!response.ok) throw await response.json();
@@ -84,23 +98,25 @@ export function NewClientModal() {
 
         <form onSubmit={handleSubmit(saveClient)}>
           <div className="grid gap-4 py-4">
-            {Object.keys(defaultValues).map((fieldName) => {
-              return (
-                <div
-                  className="grid grid-cols-4 items-center gap-4"
-                  key={fieldName}
-                >
-                  <Label htmlFor="name" className="text-right capitalize">
-                    {fieldName}
-                  </Label>
-                  <InputField
-                    field={register(fieldName as keyof NewClientForm)}
-                    containerClassName="col-span-3"
-                    errors={errors}
-                  />
-                </div>
-              );
-            })}
+            {Object.keys(defaultValues)
+              .filter((fieldName) => fieldName !== "userId")
+              .map((fieldName) => {
+                return (
+                  <div
+                    className="grid grid-cols-4 items-center gap-4"
+                    key={fieldName}
+                  >
+                    <Label htmlFor="name" className="text-right capitalize">
+                      {fieldName}
+                    </Label>
+                    <InputField
+                      field={register(fieldName as keyof NewClientForm)}
+                      containerClassName="col-span-3"
+                      errors={errors}
+                    />
+                  </div>
+                );
+              })}
           </div>
           <DialogFooter>
             <Button type="submit">Save</Button>
