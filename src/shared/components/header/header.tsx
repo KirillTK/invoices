fadd359348a517cbad1966d13b2a20c5ba1672af
Kirill Tkachyov <kirill@auxili.us>
2024-08-19 23:27:49 +0200

    #4 update link

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,10 +1,11 @@
 'use client';
+import Link from 'next/link';
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export function Header() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Invoices</div>
+      <Link href="/invoices">Invoices</Link>
       <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
