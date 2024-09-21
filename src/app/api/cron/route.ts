6e7162ed05153b29e3eed97c49eb3e31088314cb
Kirill Tkachyov <kirill@auxili.us>
2024-09-21 22:39:33 +0200

    check

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,9 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return NextResponse.json({ error: 'Unauthorized'},  { status :401 });
+  }
+
+  return NextResponse.json({ ok: true });
+}
