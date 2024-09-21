dc02771a45b92bfb2bf733cdb1c0029d248648d0
Kirill Tkachyov <kirill@auxili.us>
2024-09-21 22:42:50 +0200

    add new cron

---
+++ src/app/api/cron/currencies/route.ts
@@ -0,0 +1,12 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return NextResponse.json({ error: 'Unauthorized'},  { status :401 });
+  }
+
+
+  console.log('CURRENCIES CRON JOB CALLED');
+  
+  return NextResponse.json({ ok: true });
+}
